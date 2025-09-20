61c6595a05f9c085eca34b9f5668edd332673fb5
Izan <70977582+lz4n@users.noreply.github.com>
2025-09-20 05:29:58 +0200

    clase productos

--- src/main/java/es/upm/iwsim22_01/models/Product.java
+++ src/main/java/es/upm/iwsim22_01/models/Product.java
@@ -13,7 +13,33 @@ public class Product {
         this.price = price;
     }
 
-    
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
     public enum Category {
         MERCH,

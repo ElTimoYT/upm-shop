541f1d0fe606cbe8b5077f3e859edd0ec2521291
Izan <70977582+lz4n@users.noreply.github.com>
2025-09-23 10:36:53 +0200

    Update App.java

--- src/main/java/es/upm/iwsim22_01/App.java
+++ src/main/java/es/upm/iwsim22_01/App.java
@@ -1,10 +1,69 @@
 package es.upm.iwsim22_01;
 
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ */
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to the ticket module App.");
         System.out.println("Ticket module. Type 'help' to see commands.");
+
+        boolean menu = true;
+        String[] command;
+        while (menu) {
+            command = scanner.nextLine().split(" ");
+
+            switch (command[0]) {
+                case "exit":
+                    menu = false;
+                    break;
+                case "echo":
+                    echoMenu(command);
+                    break;
+                case "help":
+                    helpMenu();
+                    break;
+            }
+        }
+
+        System.out.println("""
+                Closing application.
+                Goodbye!
+                """);
+    }
+
+    public static void echoMenu(String[] args) {
+        for (int i = 1; i < args.length; i++) {
+            System.out.print(args[i]);
+            System.out.print(" ");
+        }
+
+        System.out.println();
+    }
+
+    public static void helpMenu() {
+        System.out.println("""
+                Commands:
+                 prod add <id> "<name>" <category> <price>
+                 prod list
+                 prod update <id> NAME|CATEGORY|PRICE <value>
+                 prod remove <id>
+                 ticket new
+                 ticket add <prodId> <quantity>
+                 ticket remove <prodId>
+                 ticket print
+                 echo "<texto>"
+                 help
+                 exit
+                 
+                Categories: MERCH, STATIONERY, CLOTHES, BOOK, ELECTRONICS
+                Discounts if there are ≥2 units in the category: MERCH 0%, STATIONERY 5%, CLOTHES 7%, BOOK 10%,
+                ELECTRONICS 3%.
+                """);
     }
 
 }

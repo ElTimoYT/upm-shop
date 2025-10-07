e44e350f5b91d45f4df26c042713ed8ee2019497
Izan <70977582+lz4n@users.noreply.github.com>
2025-10-07 09:21:29 +0200

    Update App.java

--- src/main/java/es/upm/iwsim22_01/App.java
+++ src/main/java/es/upm/iwsim22_01/App.java
@@ -22,76 +22,11 @@ public class App {
         System.out.println("Ticket module. Type 'help' to see commands.");
 
         while (menu) {
-            command = scanner.nextLine().split(" ");
-
-            switch (command[0]) {
-                case "exit":
-                    menu = false;
-                    break;
-                case "echo":
-                    echoMenu(command);
-                    break;
-                case "help":
-                    helpMenu();
-                    break;
-                case "prod":
-                    switch (command[1]) {
-                        case "add":
-                            System.out.println("prod add: " + (addProductCommand(command)?"ok":"fail"));
-                            break;
-                        case "list":
-                            System.out.println("prod list: " + (listProductsCommand(command)?"ok":"fail"));
-                            break;
-                        case "update":
-                            System.out.println("prod update: " + (updateProductCommand(command)?"ok":"fail"));
-                            break;
-                        case "remove":
-                            System.out.println("prod remove: " + (removeProductCommand(command)?"ok":"fail"));
-                            break;
-                    }
-                    break;
-                case "ticket":
-                    if (command.length < 2) {
-                        System.out.println("Use: ticket <add|remove|new|print>");
-                        break; // REVISAR BREAK DENTRO DE IF CUIDADIN
-                    }
-                    switch (command[1]) {
-                        case "add":
-                            if (command.length < 3) {
-                                System.out.println("Use: ticket add <ProdID> <Quantity>");
-                                break;
-                            }
-                            if(addTicket(command, PRODUCT_MANAGER, ticket)){
-                                System.out.println(ticket);
-                                System.out.println("Ticket add: ok");
-                            }
-                            break;
-
-                        case "remove":
-                            if (command.length < 3) {
-                                System.out.println("Use: ticket remove <ProdID>");
-                                break;
-                            }
-                            if (idExists(command, PRODUCT_MANAGER)) {
-                                ticket.removeProductById(id);
-                                System.out.println(ticket);
-                                System.out.println("Ticket remove: ok");
-                            } else {
-                                System.out.println("Product not found");
-                            }
-                            break;
-
-                        case "new":
-                            ticket = new Ticket();
-                            System.out.println("Ticket new: ok");
-                            break;
-
-                        case "print":
-                            System.out.println(ticket);
-                            System.out.println("Ticket print: ok");
-                            break;
-                    }
-                    break;
+            CommandStatus commandStatus = CommandHandler.runCommand(scanner.nextLine());
+            if (!commandStatus.getStatus()) {
+                System.err.println(commandStatus.getMessage());
+            } else if (commandStatus.getMessage() != null) {
+                System.out.println(commandStatus.getMessage());
             }
         }
 

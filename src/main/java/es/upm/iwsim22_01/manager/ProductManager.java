1c011a6f654bfb2c364fdd149bbac10ce7abe66a
sarah <sarah.soreth@gmail.co>
2025-11-11 10:12:18 +0100

    producto personalizabel

--- src/main/java/es/upm/iwsim22_01/manager/ProductManager.java
+++ src/main/java/es/upm/iwsim22_01/manager/ProductManager.java
@@ -17,6 +17,16 @@ public class ProductManager extends AbstractManager<Product, Integer> {
         return product;
     }
 
+    public Product addCustomizableProduct(int id, String name, Category category, double price, int maxPers){
+        if (!isPriceValid(price)) throw new IllegalArgumentException("Product price " + price + " cannot be negative.");
+        if (!isNameValid(name)) throw new IllegalArgumentException("Product name \"" + price + "\" is invalid.");
+        if (isProductListFull()) throw new RuntimeException("Product cannot be added, there are " + MAX_PRODUCTS + " or more products.");
+        if (maxPers < 1) throw new IllegalArgumentException("Max pers " + maxPers + " cannot be less than 1.");
+        if (existId(id)) throw new IllegalArgumentException("Product id " + id + " already exists(cannot convert basic to customizable).");
+
+        Product product = new Product(id, name, category, price, maxPers);
+    }
+
     public boolean isNameValid(String name) {
         return name != null && !name.isBlank() && name.length() < MAX_NAME_LENGTH;
     }
---
+++ src/main/java/es/upm/iwsim22_01/models/PersonalizableProduct.java
@@ -0,0 +1,11 @@
+package es.upm.iwsim22_01.models;
+
+public class PersonalizableProduct extends Product{
+    int maxPers;
+
+    public PersonalizableProduct(int id, String name, Category category, int price, int maxPers){
+        super(id, name, category, price);
+        this.maxPers = maxPers;
+    }
+    int getMaxPers(){return maxPers;}
+}
